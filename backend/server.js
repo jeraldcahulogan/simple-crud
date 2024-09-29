595d14d9d2bd8d9417ca1d5cc5ef9e6c0139adc8
MontesJC <jeraldcahulogan5@gmail.com>
2024-09-29 13:24:56 +0800

    backend part done

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,12 @@ import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000
 
 // Connect to DB and start server
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server is running on port 5000");
+    console.log(`Server is running on port ${PORT}`);
 });
 
 
